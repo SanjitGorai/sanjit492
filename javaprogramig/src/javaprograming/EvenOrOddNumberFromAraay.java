1a5f238c720bff4fd7c063a0a3d4acac5bb6ca9c
SANJIT PRASAD GORAI <SANJIT PRASAD GORAI@LAPTOP-IQ8CH363>
2023-03-14 00:35:01 +0530

    hgjhgjhgjhgjh

--- javaprogramig/src/javaprograming/EvenOrOddNumberFromAraay.java
+++ javaprogramig/src/javaprograming/EvenOrOddNumberFromAraay.java
@@ -4,7 +4,7 @@ public class EvenOrOddNumberFromAraay {
 
 	public static void main(String[] args) {
 		
-		int a[] = {1,2,3,4,5,6};
+		int a[] = {1,2,3,4,5,6};;
 		System.out.println("Even number in Array.....");
 		for(int i=0;i<a.length;i++)
 		{
