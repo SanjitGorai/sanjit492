a88b894bfe631aabbc791337aa06d896f43883b3
SANJIT PRASAD GORAI <SANJIT PRASAD GORAI@LAPTOP-IQ8CH363>
2023-03-14 00:20:27 +0530

    14-03-2023

--- javaprogramig/src/javaprograming/EvenOrOddNumberFromAraay.java
+++ javaprogramig/src/javaprograming/EvenOrOddNumberFromAraay.java
@@ -9,9 +9,9 @@ public class EvenOrOddNumberFromAraay {
 		for(int i=0;i<a.length;i++)
 		{
 			if(a[i]%2==0)
-				System.out.println(a[i]);
+				System.out.println(a[i])  ;
 		}
-		System.out.println("odd number in Array.....");
+		System.out.println("odd number in Array.....");;
 		for(int i=0;i<a.length;i++)
 		{
 			if(a[i]%2==1)
